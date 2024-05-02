b0d7d8fa4dbc162211e911431910ad76de681f93
joaovic499 <joaovictorsilva60@live.com>
2024-05-02 11:02:52 -0300

    Adicioando o serviço auth, para a validação de acesso por tokens

---
+++ src/app/auth.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private readonly JWT_TOKEN = 'JWT_TOKEN'
+  private loggedUser?: string;
+  private isAuthenticatedSubject = new BehaviorSubject<boolean>(false);
+
+  private http = inject(HttpClient);
+
+  constructor() {}
+
+  login(user:{
+    username: string, password: string
+  }): Observable<any>{
+    return this.http.post('/api/login', user).pipe(
+      tap(tokens=>this.doLoginUser(user.username, tokens))
+    )
+  }
+
+private doLoginUser(username: string, tokens: any) {
+  this.loggedUser = username;
+  this.storeJwtToken(tokens.jwt);
+  this.isAuthenticatedSubject.next((true));
+}
+
+private storeJwtToken(jwt: string){
+  localStorage.setItem(this.JWT_TOKEN, jwt);
+}
+
+logout(){
+  localStorage.removeItem(this.JWT_TOKEN);
+  this.isAuthenticatedSubject.next(false);
+}
+}
